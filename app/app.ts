89dc90fa94640b6dfaf4f4c31677287eb0efb381
Sergey Kononenko <hackfeed@yandex.ru>
2020-04-21 21:36:32 +0300

    Introduction to tuples and enums

--- app/app.ts
+++ app/app.ts
@@ -1,8 +1,18 @@
+enum Role {
+  ADMIN = 0,
+  READ_ONLY,
+  AUTHOR,
+}
+
 const person = {
   name: "Sergey",
   age: 20,
-  hobbies: ["Sports", "Cooking"]
+  hobbies: ["Sports", "Cooking"],
+  role: Role.ADMIN,
 };
+
+// person.role.push("admin");
+// person.role[1] = 10;
 
 let favoriteActivities: string[];
 favoriteActivities = ["Sports"];
