136af8f4b7c62ab17064c45e276435a95c4e7975
Sergey Kononenko <hackfeed@yandex.ru>
2020-04-23 03:58:13 +0300

    More on types in callbacks

--- app/app.ts
+++ app/app.ts
@@ -6,8 +6,17 @@ function printResult(res: number) {
   console.log("Result: " + res);
 }
 
+function addAndHandle(n1: number, n2: number, cb: (a: number) => void) {
+  const result = n1 + n2;
+  cb(result);
+}
+
 printResult(add(1, 3));
 
 let combineValues: (a: number, b: number) => number = add;
 
 console.log(combineValues(3, 2));
+
+addAndHandle(5, 7, (result) => {
+  console.log(result);
+});
