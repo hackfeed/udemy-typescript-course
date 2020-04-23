f31961cd3710ac414e1a5ad9929d5db14987c000
Sergey Kononenko <hackfeed@yandex.ru>
2020-04-23 03:09:06 +0300

    Introduction to literal types

--- app/app.ts
+++ app/app.ts
@@ -1,15 +1,25 @@
-function combine(input1: number | string, input2: number | string) {
+function combine(
+  input1: number | string,
+  input2: number | string,
+  resultConversion: "as-number" | "as-string"
+) {
   let result;
-  if (typeof input1 === "number" && typeof input2 === "number") {
-    result = input1 + input2;
+  if (
+    (typeof input1 === "number" && typeof input2 === "number") ||
+    resultConversion === "as-number"
+  ) {
+    result = +input1 + +input2;
   } else {
     result = input1.toString() + input2.toString();
   }
   return result;
 }
 
-const combinedAges = combine(30, 26);
+const combinedAges = combine(30, 26, "as-number");
 console.log(combinedAges);
 
-const combinedNames = combine("Max", "Anna");
+const combinedAgesString = combine("30", "26", "as-number");
+console.log(combinedAgesString);
+
+const combinedNames = combine("Max", "Anna", "as-string");
 console.log(combinedNames);
