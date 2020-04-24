252ac253b556b85d17215923f7c31eea3ed53b9e
Sergey Kononenko <hackfeed@yandex.ru>
2020-04-24 14:59:16 +0300

    Add get & set for Accounting department

--- 02_oop/src/app.ts
+++ 02_oop/src/app.ts
@@ -1,5 +1,5 @@
 class Department {
-  private employees: string[] = [];
+  protected employees: string[] = [];
 
   constructor(private readonly id: string, public name: string) {}
 
@@ -27,12 +27,37 @@ class ITDepartment extends Department {
 }
 
 class AccountingDepartment extends Department {
+  private lastReport: string;
+
+  get mostRecentReport() {
+    if (this.lastReport) {
+      return this.lastReport;
+    }
+    throw new Error("No report found.");
+  }
+
+  set mostRecentReport(value: string) {
+    if (!value) {
+      throw new Error("Please pass in a valid value!");
+    }
+    this.addReport(value);
+  }
+
   constructor(id: string, private reports: string[]) {
     super(id, "Accounting");
+    this.lastReport = this.reports[0];
+  }
+
+  addEmployee(employee: string) {
+    if (employee === "Max") {
+      return;
+    }
+    this.employees.push(employee);
   }
 
   addReport(text: string) {
     this.reports.push(text);
+    this.lastReport = text;
   }
 
   printReports() {
@@ -51,6 +76,9 @@ it.printEmployeeInformation();
 console.log(it);
 
 const accounting = new AccountingDepartment("d2", []);
+
+accounting.mostRecentReport = "Year end report";
+console.log(accounting.mostRecentReport);
 
 accounting.addReport("Something went wrong...");
 accounting.printReports();
