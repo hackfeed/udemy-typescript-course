ef08d0178ad0016aba6dc9f6b288b3d9280da036
Sergey Kononenko <hackfeed@yandex.ru>
2020-04-24 20:30:34 +0300

    Introduction to abstract classes

--- 02_oop/src/app.ts
+++ 02_oop/src/app.ts
@@ -1,11 +1,13 @@
-class Department {
+abstract class Department {
   protected employees: string[] = [];
 
-  constructor(private readonly id: string, public name: string) {}
+  constructor(protected readonly id: string, public name: string) {}
 
-  describe(this: Department) {
-    console.log(`Department (${this.id}): ${this.name}`);
+  static createEmployee(name: string) {
+    return { name: name };
   }
+
+  abstract describe(this: Department): void;
 
   addEmployee(employee: string) {
     this.employees.push(employee);
@@ -24,10 +26,15 @@ class ITDepartment extends Department {
     super(id, "IT");
     this.admins = admins;
   }
+
+  describe() {
+    console.log("IT Department with ID " + this.id);
+  }
 }
 
 class AccountingDepartment extends Department {
   private lastReport: string;
+  private static instance: AccountingDepartment;
 
   get mostRecentReport() {
     if (this.lastReport) {
@@ -43,9 +50,17 @@ class AccountingDepartment extends Department {
     this.addReport(value);
   }
 
-  constructor(id: string, private reports: string[]) {
+  private constructor(id: string, private reports: string[]) {
     super(id, "Accounting");
     this.lastReport = this.reports[0];
+  }
+
+  static getInstance() {
+    if (AccountingDepartment.instance) {
+      return this.instance;
+    }
+    this.instance = new AccountingDepartment("d2", []);
+    return this.instance;
   }
 
   addEmployee(employee: string) {
@@ -63,7 +78,14 @@ class AccountingDepartment extends Department {
   printReports() {
     console.log(this.reports);
   }
+
+  describe() {
+    console.log("Accounting Department with ID " + this.id);
+  }
 }
+
+const employee1 = Department.createEmployee("max");
+console.log(employee1);
 
 const it = new ITDepartment("d1", ["Max"]);
 
@@ -75,10 +97,11 @@ it.printEmployeeInformation();
 
 console.log(it);
 
-const accounting = new AccountingDepartment("d2", []);
+const accounting = AccountingDepartment.getInstance();
 
 accounting.mostRecentReport = "Year end report";
 console.log(accounting.mostRecentReport);
 
+accounting.describe();
 accounting.addReport("Something went wrong...");
 accounting.printReports();
