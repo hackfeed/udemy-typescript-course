967ccb1427181cde64109e33953b704c107ffe05
Sergey Kononenko <hackfeed@yandex.ru>
2020-04-24 04:31:49 +0300

    Reorganize repo & add classes introduction

---
+++ 02_oop/src/app.ts
@@ -0,0 +1,15 @@
+class Department {
+  name: string;
+
+  constructor(name: string) {
+    this.name = name;
+  }
+
+  describe(this: Departments) {
+    console.log("Department: " + this.name);
+  }
+}
+
+const accounting = new Department("Accounting");
+
+accounting.describe();
