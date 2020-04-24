6455e34981d7cbd1fce2d105dad55cb01235f559
Sergey Kononenko <hackfeed@yandex.ru>
2020-04-24 13:56:13 +0300

    Introduction to private fields

--- 02_oop/src/app.ts
+++ 02_oop/src/app.ts
@@ -1,5 +1,6 @@
 class Department {
-  name: string;
+  private name: string;
+  private employees: string[] = [];
 
   constructor(name: string) {
     this.name = name;
@@ -8,8 +9,21 @@ class Department {
   describe(this: Department) {
     console.log("Department: " + this.name);
   }
+
+  addEmployee(employee: string) {
+    this.employees.push(employee);
+  }
+
+  printEmployeeInformation() {
+    console.log(this.employees.length);
+    console.log(this.employees);
+  }
 }
 
 const accounting = new Department("Accounting");
 
+accounting.addEmployee("Max");
+accounting.addEmployee("Manu");
+
 accounting.describe();
+accounting.printEmployeeInformation();
