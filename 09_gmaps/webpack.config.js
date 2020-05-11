1e5a90160caeb2f0b23b914395f45d0b69bddc66
Sergey Kononenko <hackfeed@yandex.ru>
2020-05-11 16:11:34 +0300

    Add GMaps project

---
+++ 09_gmaps/webpack.config.js
@@ -0,0 +1,24 @@
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/app.ts",
+  output: {
+    filename: "app.js",
+    path: path.resolve(__dirname, "dist"),
+    publicPath: "dist",
+  },
+  devtool: "inline-source-map",
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".ts", ".js"],
+  },
+};
